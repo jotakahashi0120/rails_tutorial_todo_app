1d1fc74c5a81bc400f02397d24b2802a0ea831e4
JoTakahashi <jo.takahashi.0120@gmail.com>
2020-11-22 17:48:01 +0000

    [Add]tasks#createを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,4 +10,18 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def task_params
+      params.require(:task).permit(:name)
+    end
 end
