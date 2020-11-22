18ed38bbcbfa8628987b726e35f14726332e919d
JoTakahashi <jo.takahashi.0120@gmail.com>
2020-11-22 16:43:01 +0000

    [Update]rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201122164104) do
+ActiveRecord::Schema.define(version: 20201122164227) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
